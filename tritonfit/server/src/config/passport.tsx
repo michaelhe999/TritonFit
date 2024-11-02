597fbc91e8ae04174e912edb4b52e286db1c56af
Michael He <michaelhe2016@gmail.com>
2024-11-02 03:08:20 -0700

    First Repository commit! Created general MERN framework, started work on Google OAuth

---
+++ tritonfit/server/src/config/passport.tsx
@@ -0,0 +1,47 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import User from '../models/User';
+
+passport.serializeUser((user: any, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id: string, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: '/auth/google/callback',
+      scope: ['profile', 'email']
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const existingUser = await User.findOne({ googleId: profile.id });
+        
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+
+        const newUser = await User.create({
+          googleId: profile.id,
+          email: profile.emails![0].value,
+          name: profile.displayName,
+          picture: profile.photos![0].value
+        });
+
+        done(null, newUser);
+      } catch (error) {
+        done(error as Error, undefined);
+      }
+    }
+  )
+);
