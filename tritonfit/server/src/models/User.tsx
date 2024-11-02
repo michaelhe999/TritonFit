5499efc739123739765d7f8670591d8c9fcc6d5f
Michael He <michaelhe2016@gmail.com>
2024-11-02 03:08:20 -0700

    First Repository commit! Created general MERN framework, started work on Google OAuth

---
+++ tritonfit/server/src/models/User.tsx
@@ -0,0 +1,30 @@
+import mongoose, { Document } from 'mongoose';
+
+export interface IUser extends Document {
+  googleId: string;
+  email: string;
+  name: string;
+  picture: string;
+  createdAt: Date;
+}
+
+const userSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: String,
+  picture: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model<IUser>('User', userSchema);
