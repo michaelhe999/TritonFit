730cac9a931a2d35966d1aa404893c15794e08ce
emmaz12 <ezc7288@gmail.com>
2024-11-12 19:41:43 -0800

    Update App.tsx

--- tritonfit/src/App.tsx
+++ tritonfit/src/App.tsx
@@ -1,10 +1,15 @@
-import { FindAWorkout } from "./views/FindAWorkout";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import './App.css';
 
-const App = () => {
-  // HINT: Wrap the MyBudgetTracker component with AppContextProvider
+function App() {
   return (
-		<FindAWorkout />
-  )
-};
+  <Router>
+    <Routes>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </Router>
+  );
+}
 
 export default App;
