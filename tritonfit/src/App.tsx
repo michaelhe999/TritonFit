7fdddcdd6e72c628a71f8e8c4831ed1eafd8db0c
emmaz12 <ezc7288@gmail.com>
2024-11-14 00:22:51 -0800

    update route to find a workout page on navbar
    update route to find a workout page on navbar

--- tritonfit/src/App.tsx
+++ tritonfit/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Navbar/Navbar";
 import Home from "./components/Home";
-import FindWorkout from "./components/FindWorkout";
+import { FindAWorkout } from "./views/FindAWorkout";
 import MeetOthers from "./components/MeetOthers";
 import Profile from "./components/Profile";
 
@@ -13,7 +13,7 @@ const App: React.FC = () => {
       <div className="app-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/findworkout" element={<FindWorkout />} />
+          <Route path="/findworkout" element={<FindAWorkout />} />
           <Route path="/meetothers" element={<MeetOthers />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
@@ -25,3 +25,43 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+
+/**
+ * import React, { useEffect, useState } from 'react';
+import { generateWorkout } from './utils/workoutGenerator-utils';
+import { WorkoutFormResponses, WorkoutList } from './types/workout';
+const sampleResponse: WorkoutFormResponses = {
+  gender: 'female',
+  goal: 'full body',
+  targetArea: 'back',
+  level: 'intermediate',
+  duration: '60'
+}
+
+async function test() {
+  const response = await generateWorkout(sampleResponse);
+  return response;
+}
+
+function App() {
+  // Define a state variable to store the resolved response
+  const [workout, setWorkout] = useState<WorkoutList>();
+
+  useEffect(() => {
+    // Call the async function inside useEffect to avoid calling it directly in the component body
+    test().then(response => {
+      setWorkout(response);  // Store the resolved response in state
+    });
+  }, []);
+
+  return (
+    <div>
+      {workout ? <pre>{JSON.stringify(workout, null, 2)}</pre> : 'Loading...'}
+    </div>
+  );
+}
+
+export default App;
+
+ */
