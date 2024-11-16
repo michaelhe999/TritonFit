0c0d219024e926d415526fd2ce56e61547eb7d96
NicholasDroppa <100443908+NicholasDroppa@users.noreply.github.com>
2024-11-16 11:49:57 -0800

    Added state tracking to profile edit page

--- tritonfit/src/views/ProfilePage.tsx
+++ tritonfit/src/views/ProfilePage.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import styles from './ProfilePage.module.css';
 import userIcon from "../assets/userIcon.png"
 import backIcon from "../assets/backIcon.svg"
@@ -8,12 +9,52 @@ interface ProfileEditProps {
 }
 
 export const ProfilePage: React.FC<ProfileEditProps> = ({onBackButton}: ProfileEditProps) => {
+	const [firstName, setFirstName] = useState("Jane");
+	const [lastName, setLastName] = useState("Doe");
+	const [email, setEmail] = useState("janedoe@gmail.com");
+	const [major, setMajor] = useState("Bioengineering Junior");
+	const [experience, setExperience] = useState("Running, Aerobics");
+	const [about, setAbout] = useState("Hi! I'm just a regular Jane Doe.");
+
+	const updateFirstName = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setFirstName(event.target.value)
+	}
+
+	const updateLastName = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setLastName(event.target.value)
+	}
+
+	const updateEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setEmail(event.target.value)
+	}
+
+	const updateMajor = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setMajor(event.target.value)
+	}
+
+	const updateExperience = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setExperience(event.target.value)
+	}
+
+	const updateAbout = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setAbout(event.target.value)
+	}
+
+	const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+        alert(
+            "Save changes.\n\n" +
+            "First name: " + firstName + "\n" +
+            "Last name: " + lastName + "\n" +
+            "Email: " + email + "\n" +
+            "Major: " + major + "\n" +
+            "Experience: " + experience + "\n" +
+            "About: " + about
+        );
+	};
+
     const editIcon = () => {
         alert("Edit profile icon.");
-    };
-
-    const saveChanges = () => {
-        alert("Save changes.");
     };
 
     const deleteAccount = () => {
@@ -26,20 +67,56 @@ export const ProfilePage: React.FC<ProfileEditProps> = ({onBackButton}: ProfileE
             <img className={styles.icon} src={userIcon} alt="icon"/>
             <input className={styles.backButton} type="image" src={backIcon} onClick={onBackButton}/>
             <input className={styles.editIconButton} type="button" value="Tap to edit" onClick={editIcon}/>
-            <form className={styles.inputForm}>
+            <form className={styles.inputForm} onSubmit={(event) => onSubmit(event)}>
                 <label className={styles.inputFormLabel} htmlFor="firstNameInput">First name</label>
-                <input className={styles.inputFormField} type="text" name="firstNameInput"/>
+                <input
+                    className={styles.inputFormField}
+                    type="text"
+                    name="firstNameInput"
+					value={firstName}
+					onChange={updateFirstName}
+                />
                 <label className={styles.inputFormLabel} htmlFor="lastNameInput">Last name</label>
-                <input className={styles.inputFormField} type="text" name="lastNameInput"/>
+                <input
+                    className={styles.inputFormField}
+                    type="text"
+                    name="lastNameInput"
+					value={lastName}
+					onChange={updateLastName}
+                />
                 <label className={styles.inputFormLabel} htmlFor="emailInput">Email address</label>
-                <input className={styles.inputFormField} type="text" name="emailInput"/>
+                <input
+                    className={styles.inputFormField}
+                    type="text"
+                    name="emailInput"
+					value={email}
+					onChange={updateEmail}
+                />
                 <label className={styles.inputFormLabel} htmlFor="majorInput">Major and year</label>
-                <input className={styles.inputFormField} type="text" name="majorInput"/>
+                <input
+                    className={styles.inputFormField}
+                    type="text"
+                    name="majorInput"
+					value={major}
+					onChange={updateMajor}
+                />
                 <label className={styles.inputFormLabel} htmlFor="experienceInput">Experience in Working Out</label>
-                <input className={styles.inputFormField} type="text" name="experienceInput"/>
+                <input
+                    className={styles.inputFormField}
+                    type="text"
+                    name="experienceInput"
+					value={experience}
+					onChange={updateExperience}
+                />
                 <label className={styles.inputFormLabel} htmlFor="aboutInput">About me</label>
-                <textarea className={styles.inputAbout} name="aboutInput" rows={5}/>
-                <button className={styles.submit} type="submit" onClick={saveChanges}>Save Changes</button>
+                <textarea
+                    className={styles.inputAbout}
+                    name="aboutInput"
+                    rows={5}
+					value={about}
+					onChange={updateAbout}
+                />
+                <button className={styles.submit} type="submit">Save Changes</button>
             </form>
             <button className={styles.delete} onClick={deleteAccount}>
                 <img className={styles.deleteIcon} src={deleteIcon} alt="icon"/>
