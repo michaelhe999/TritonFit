228c70d2fc8d0fce723e6343c3a16baae12f9979
Meshall <malshalaan@ucsd.edu>
2024-11-19 13:14:36 -0800

    updated routes to include signin and createaccount

--- tritonfit/src/App.tsx
+++ tritonfit/src/App.tsx
@@ -6,6 +6,8 @@ import Home from "./components/Home";
 import { FindAWorkout } from "./views/FindAWorkout";
 import MeetOthers from "./components/MeetOthers";
 import Profile from "./components/Profile";
+import { SignIn } from "./components/signIn";
+import { CreateAccount } from "./components/createAccount";
 
 const App: React.FC = () => {
   return (
@@ -16,6 +18,9 @@ const App: React.FC = () => {
           <Route path="/findworkout" element={<FindAWorkout />} />
           <Route path="/meetothers" element={<MeetOthers />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/createaccount" element={<CreateAccount />} />
+
         </Routes>
       </div>
 
