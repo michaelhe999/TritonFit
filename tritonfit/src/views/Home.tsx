25bdc530ec65da13951cd632816651dac4840c7d
mmc003 <mingyangmchu@gmail.com>
2024-11-19 17:59:13 -0800

    trend button removed and css updates

--- tritonfit/src/views/Home.tsx
+++ tritonfit/src/views/Home.tsx
@@ -12,10 +12,10 @@ const Home: React.FC = () => {
   const hoursData: { [day: string]: number } = {
     Sun: 0,
     Mon: 2,
-    Tue: 3,
-    Wed: 0,
+    Tue: 4,
+    Wed: 2,
     Thu: 5,
-    Fri: 2,
+    Fri: 0,
     Sat: 0,
   };
 
@@ -32,23 +32,23 @@ const Home: React.FC = () => {
     <div className="container">
       <header className="header">
         <h1>Welcome {user}</h1>
-        <div className="header-icons">
-          <button
+        {/* <div className="header-icons"> */}
+          {/* <button
             className="header-button"
             onClick={() => alert("Trends icon clicked!")}
             aria-label="Trends"
           >
             <img src={Trends} alt="" />
-          </button>
+          </button> */}
 
-          <button
+          {/* <button
             className="header-button"
             onClick={() => alert("Notification icon clicked!")}
             aria-label="Notification"
           >
-            <img src={Notification} alt="" />
-          </button>
-        </div>
+            <img src={Notification} alt="" /> */}
+          {/* </button> */}
+        {/* </div> */}
       </header>
 
       <div className="section">
