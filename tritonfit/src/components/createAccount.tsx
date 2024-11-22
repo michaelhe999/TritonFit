5b30b9bd0dbee7c6af5fabcd08ad5ee7e33e596c
Meshall <malshalaan@ucsd.edu>
2024-11-22 14:40:42 -0800

    CSS conflicts resolved and improved

--- tritonfit/src/components/createAccount.tsx
+++ tritonfit/src/components/createAccount.tsx
@@ -1,128 +1,137 @@
 import { useState } from "react";
-import '../views/createAccount.css'
 import userPNG from '../assets/userPNG.png';
-export const CreateAccount = () => {
+import styles from '../views/createAccount.module.css'; // Import the CSS Module
 
+export const CreateAccount = () => {
   const initialUser = {
     id: -1,
-    firstName: "",
-    lastName: "",
-    email: "",
-    major: "",
-    year: "",
-    experience: "",
-    aboutMe: "",
+    firstName: '',
+    lastName: '',
+    email: '',
+    major: '',
+    year: '',
+    experience: '',
+    aboutMe: '',
   };
 
   const [createUser, setCreateUser] = useState(initialUser);
 
-
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    //For Demo Purposes
-    alert("First Name: " + createUser.firstName + "\n" +
-          "Last Name: " + createUser.lastName + "\n" +
-          "Email: " + createUser.email + "\n" +
-          "Major: " + createUser.major + "\n" +
-          "Year: " + createUser.year + "\n" +
-          "Experience: " + createUser.experience + "\n" +
-          "About Me: " + createUser.aboutMe
+    // For demo purposes
+    alert(
+      'First Name: ' +
+        createUser.firstName +
+        '\n' +
+        'Last Name: ' +
+        createUser.lastName +
+        '\n' +
+        'Email: ' +
+        createUser.email +
+        '\n' +
+        'Major: ' +
+        createUser.major +
+        '\n' +
+        'Year: ' +
+        createUser.year +
+        '\n' +
+        'Experience: ' +
+        createUser.experience +
+        '\n' +
+        'About Me: ' +
+        createUser.aboutMe
     );
     setCreateUser(initialUser);
   };
- 
- 
- 
+
   return (
-    <form onSubmit={(event) => handleSubmit(event)}>
-      
-    <div>
-      <h1>Create An Account</h1>
-      <img src={userPNG} className="form-logo"/>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        type="text"
-        id="firstName"
-        name="firstName"
-        value={createUser.firstName}
-        onChange={(event) =>
-          setCreateUser({ ...createUser, firstName: event.target.value})}
-        required/>
-    </div>
+    <form onSubmit={(event) => handleSubmit(event)} className={styles.createAccount}>
+      <h1>Create an Account</h1>
+      <img src={userPNG} className={styles.formLogo} alt="User Logo" />
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={createUser.firstName}
+          onChange={(event) => setCreateUser({ ...createUser, firstName: event.target.value })}
+          required
+        />
+      </div>
 
-    <div>
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        type="text"
-        id="lastName"
-        name="lastName"
-        value={createUser.lastName}
-        onChange={(event) =>
-          setCreateUser({ ...createUser, lastName: event.target.value})}
-        required/>
-    </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={createUser.lastName}
+          onChange={(event) => setCreateUser({ ...createUser, lastName: event.target.value })}
+          required
+        />
+      </div>
 
-    <div>
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        value={createUser.email}
-        onChange={(event) =>
-          setCreateUser({ ...createUser, email: event.target.value})}
-        required/>
-    </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={createUser.email}
+          onChange={(event) => setCreateUser({ ...createUser, email: event.target.value })}
+          required
+        />
+      </div>
 
-    <div>
-      <label htmlFor="major">Major</label>
-      <input
-        type="text"
-        id="major"
-        name="major"
-        value={createUser.major}
-        onChange={(event) =>
-          setCreateUser({ ...createUser, major: event.target.value})}
-        required/>
-    </div>
+      <div>
+        <label htmlFor="major">Major</label>
+        <input
+          type="text"
+          id="major"
+          name="major"
+          value={createUser.major}
+          onChange={(event) => setCreateUser({ ...createUser, major: event.target.value })}
+          required
+        />
+      </div>
 
-    <div>
-      <label htmlFor="year">Year</label>
-      <input
-        type="text"
-        id="year"
-        name="year"
-        value={createUser.year}
-        onChange={(event) =>
-          setCreateUser({ ...createUser, year: (event.target.value)})}
-        required/>
-    </div>
+      <div>
+        <label htmlFor="year">Year</label>
+        <input
+          type="text"
+          id="year"
+          name="year"
+          value={createUser.year}
+          onChange={(event) => setCreateUser({ ...createUser, year: event.target.value })}
+          required
+        />
+      </div>
 
-    <div>
-      <label htmlFor="experience">Experience Working Out</label>
-      <input
-        type="text"
-        id="experience"
-        name="experience"
-        value={createUser.experience}
-        onChange={(event) =>
-          setCreateUser({ ...createUser, experience: event.target.value})}
-        required/>
-    </div>
+      <div>
+        <label htmlFor="experience">Experience Working Out</label>
+        <input
+          type="text"
+          id="experience"
+          name="experience"
+          value={createUser.experience}
+          onChange={(event) => setCreateUser({ ...createUser, experience: event.target.value })}
+          required
+        />
+      </div>
 
-    <div>
-      <label htmlFor="aboutMe">About Me</label>
-      <textarea
-        id="aboutMe"
-        name="aboutMe"
-        value={createUser.aboutMe}
-        onChange={(event) =>
-          setCreateUser({ ...createUser, aboutMe: event.target.value})}
-        required
-      ></textarea>
-    </div>
+      <div>
+        <label htmlFor="aboutMe">About Me</label>
+        <textarea
+          id="aboutMe"
+          name="aboutMe"
+          value={createUser.aboutMe}
+          onChange={(event) => setCreateUser({ ...createUser, aboutMe: event.target.value })}
+          required
+        ></textarea>
+      </div>
 
-    <button type="submit">Create Account</button>
-  </form>
+      <button type="submit">Create Account</button>
+    </form>
   );
 };
--- tritonfit/src/components/signIn.tsx
+++ tritonfit/src/components/signIn.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
-import '../views/signIn.css'
 import appLogo from '../assets/appLogo.png';
+import styles from '../views/signIn.module.css';
+
 
 export const SignIn = () => {
   const navigate = useNavigate();
@@ -9,13 +10,14 @@ export const SignIn = () => {
     navigate('/createAccount')
   }
 
-
   return (
-    <div>
-      <img src={appLogo} className="app-logo"/>
-      <h1>TritonFit</h1>
-      <button onClick={handleSubmit}> Sign in with Google </button>
+    <div className={styles.SignIn}>
+      <img src={appLogo} alt="App Logo" className={styles['app-logo']} />
+      <h1 className={styles.title}>TritonFit</h1>
+      <button  onClick={handleSubmit} className={styles.button}>
+        Sign in with Google
+      </button>
     </div>
-    
-  )
+  );
 };
+
