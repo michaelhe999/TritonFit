da75a45cb8098a4280df26d4532bee661ba0ea11
NicholasDroppa <100443908+NicholasDroppa@users.noreply.github.com>
2024-11-23 02:03:38 -0800

    Added new features to profile tab: local profile picture upload, sign out button with popup, and link to TritonFit on GitHub

--- tritonfit/src/App.tsx
+++ tritonfit/src/App.tsx
@@ -6,7 +6,8 @@ import Navbar from "./Navbar/Navbar";
 import Home from "./views/Home";
 import { FindAWorkout } from "./views/FindAWorkout";
 import MeetOthers from "./components/dummy-pages/MeetOthers";
-import Profile from "./components/dummy-pages/Profile";
+import { ProfileTab } from "./views/ProfileTab";
+import { ProfilePage } from "./views/ProfilePage";
 import { RecommendedWorkouts } from "./views/RecommendedWorkouts";
 import { ExercisesPage } from "./components/ExercisesPage";
 import { CreateAccount } from "./views/createAccount";
@@ -15,50 +16,59 @@ import { SignIn } from "./views/signIn";
 const App: React.FC = () => {
   return (
     <Router>
-      {/* Navbar included for these routes */}
-      <Routes>
-        <Route path="/" 
-          element={
-            <>
-              <Home />
-              <Navbar />
-            </>
-          }
-        />
-        <Route path="/findworkout"
-          element={
-            <>
-              <FindAWorkout />
-              <Navbar />
-            </>
-          }
-        />
-        <Route path="/meetothers"
-          element={
-            <>
-              <MeetOthers />
-              <Navbar />
-            </>
-          }
-        />
-        <Route path="/profile"
-          element={
-            <>
-              <Profile />
-              <Navbar />
-            </>
-          }
-        />
-      </Routes>
+      <div className="app-container">
+        {/* Navbar included for these routes */}
+        <Routes>
+          <Route path="/" 
+            element={
+              <>
+                <Home />
+                <Navbar />
+              </>
+            }
+            />
+            <Route path="/findworkout"
+            element={
+              <>
+                <FindAWorkout />
+                <Navbar />
+              </>
+            }
+            />
+            <Route path="/meetothers"
+            element={
+              <>
+                <MeetOthers />
+                <Navbar />
+              </>
+            }
+            />
+            <Route path="/profile"
+            element={
+              <div className="profileView">
+                <ProfileTab />
+                <Navbar />
+              </div>
+            }
+            />
+        </Routes>
 
-      {/* Routes that don't include the Navbar */}
-      <Routes>
-        <Route path="/recommendedWorkouts" element={<RecommendedWorkouts />} />
-        <Route path="/exercises" element={<ExercisesPage />} />
-        <Route path="/createaccount" element={<CreateAccount />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/createWorkout" element={<CreateWorkout />} />
-      </Routes>
+        {/* Routes that don't include the Navbar */}
+        <Routes>
+            <Route path="/recommendedWorkouts" element={<RecommendedWorkouts />} />
+            <Route path="/exercises" element={<ExercisesPage />} />
+            <Route path="/createaccount" element={<CreateAccount />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/createWorkout" element={<CreateWorkout />} />
+            <Route path="/edit-profile"
+            element={
+              <div className="profileView">
+                <ProfilePage />
+              </div>
+            }
+            />
+        </Routes>
+      </div>
     </Router>
 
   );
